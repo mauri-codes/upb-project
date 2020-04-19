ff57c7fa96414d0b343b6a19ccc952f21acd2435
EC2 Default User <ec2-user@ip-172-31-45-159.us-east-2.compute.internal>
2020-04-19 02:31:35 +0000

    fixed update, create, delete commands

--- deploy.py
+++ deploy.py
@@ -2,12 +2,12 @@
 import getopt, sys
 
 argument_list = sys.argv[1:]
-short_options = "ho:v"
+short_options = "cdus"
 long_options = ["create", "delete", "update", "set"]
 
 try:
     arguments, values = getopt.getopt(argument_list, short_options, long_options)
-    command = arguments[0][0].split('--')[1]
+    command = arguments[0][0]
 except getopt.error as err:
     print (str(err))
     sys.exit(2)
@@ -20,35 +20,40 @@ cf = boto3.client('cloudformation')
 with open('template.yaml', 'r') as file:
     template = file.read()
 
-project = "upb-cf"
+project = "upb-cf3"
 
-if command == "create":
+if command in ("--create", "-c"):
+
     create_stack_response = cf.create_stack(
         StackName=project,
         TemplateBody=template
     )
     print(create_stack_response)
 
-elif command == "update":
+elif command in ("--update", "-u"):
+
     update_stack_response = cf.update_stack(
         StackName=project,
         TemplateBody=template
     )
     print(update_stack_response)
 
-elif command == "delete":
+elif command in ("--delete", "-d"):
+
     delete_stack_response = cf.delete_stack(
         StackName=project
     )
+    print(delete_stack_response)
 
-elif command == "set":
+elif command in ("--set", "-s"):
+
     change_set_response = cf.create_change_set(
         StackName=project,
         TemplateBody=template,
         ChangeSetName="uniquestring"
     )
     print(change_set_response)
-    
+
 else:
     print("No command recognized")
 
