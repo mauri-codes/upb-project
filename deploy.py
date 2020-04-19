393eaa73ac049a31d86b636d2f6c4eb1b699a79b
EC2 Default User <ec2-user@ip-172-31-45-159.us-east-2.compute.internal>
2020-04-19 02:07:17 +0000

    added commands to deploy.py

--- deploy.py
+++ deploy.py
@@ -1,29 +1,55 @@
+#!/usr/bin/python3
+import getopt, sys
+
+argument_list = sys.argv[1:]
+short_options = "ho:v"
+long_options = ["create", "delete", "update", "set"]
+
+try:
+    arguments, values = getopt.getopt(argument_list, short_options, long_options)
+    command = arguments[0][0].split('--')[1]
+except getopt.error as err:
+    print (str(err))
+    sys.exit(2)
+
+###############################################################################
 import boto3
-project = "upb-cf"
 
 cf = boto3.client('cloudformation')
 
 with open('template.yaml', 'r') as file:
     template = file.read()
 
-create_stack_response = cf.create_stack(
-    StackName=project,
-    TemplateBody=template
-)
+project = "upb-cf"
 
-# create_stack_response = cf.create_change_set(
-#     StackName=project,
-#     TemplateBody=template,
-#     ChangeSetName="hiworld"
-# )
+if command == "create":
+    create_stack_response = cf.create_stack(
+        StackName=project,
+        TemplateBody=template
+    )
+    print(create_stack_response)
 
-# create_stack_response = cf.delete_stack(
-#     StackName=project
-# )
+elif command == "update":
+    update_stack_response = cf.update_stack(
+        StackName=project,
+        TemplateBody=template
+    )
+    print(update_stack_response)
 
-# create_stack_response = cf.update_stack(
-#     StackName=project,
-#     TemplateBody=template
-# )
+elif command == "delete":
+    delete_stack_response = cf.delete_stack(
+        StackName=project
+    )
 
-print(create_stack_response)
+elif command == "set":
+    change_set_response = cf.create_change_set(
+        StackName=project,
+        TemplateBody=template,
+        ChangeSetName="uniquestring"
+    )
+    print(change_set_response)
+    
+else:
+    print("No command recognized")
+
+
